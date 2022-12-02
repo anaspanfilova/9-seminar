8d0667229478021787537a5c2193d0349f2c450f
Анастасия <understooooood@mail.ru>
2022-12-02 18:16:40 +0300

    решила 66 задачу

--- 66/Program.cs
+++ 66/Program.cs
@@ -1,11 +1,6 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-
-Console.WriteLine("Введите целое положительное число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите целое положительное число N: ");
-int n = int.Parse(Console.ReadLine()!);
  
 void GapNumberSum (int num1, int num2, int sum)
 {
@@ -17,5 +12,11 @@ void GapNumberSum (int num1, int num2, int sum)
     sum = sum + (num1++);
     GapNumberSum(num1, num2, sum);
 }
+
+Console.Clear();
+Console.WriteLine("Введите целое положительное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N: ");
+int n = int.Parse(Console.ReadLine()!);
 
 GapNumberSum(m, n, 0);
